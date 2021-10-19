ba4ea4b70743a939586bbf75fb573d1031620d40
Al Nadim <92807227+Al-Nadim@users.noreply.github.com>
2021-10-19 22:15:42 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# This tool make by AX007
+
+import os
+import subprocess
+from subprocess import check_call
+
+os.system ("apt update")
+os.system ("apt upgrade")
+os.system ("termux-setup-storage")
+os.system ("apt install git")
+os.system ("apt install python")
+os.system ("apt install python2")
+os.system ("apt install nano")
+os.system ("apt install fish")
+os.system ("apt install figlet")
+os.system ("apt install cmatrix")
+os.system ("apt install toilet")
+os.system ("apt install ruby")
+os.system ("apt install php")
+os.system ("apt install openssh")
+os.system ("apt install wget")
+os.system ("apt install proof")
+os.system ("apt install proot")
+os.system ("apt install lolcat")
+os.system ("pip install pip")
+os.system ("pip install future")
+os.system ("pip install pip2")
+os.system ("pip2 install requests")
+os.system ("pip2 install machanize")
+os.system ("pip2 install requirements")
+
